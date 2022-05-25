e242ee3e56008159bdbaa1f1ffcca93854f83553
Avily Silva <avily.sva@gmail.com>
2022-05-25 16:35:47 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home(): JSX.Element {
+  return (
+   <div>
+     <h1>OnMovies</h1>
+   </div>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        color: "gray.400",
+        bg: "gray.900"
+      }
+    }
+  }
+});
